a3c935706295c2c63d508c77a15283711e67d632
DanVila <131091182+DanVila@users.noreply.github.com>
2023-04-25 18:50:53 -0600

    Update ejercicio_001.py

--- ejercicio_001.py
+++ ejercicio_001.py
@@ -1,21 +1,21 @@
 import datetime
 
-nombre='Daniel'
-apellido1 = 'Villa'
-apellido2 = 'Corona'
-año_nacimiento = '1993'
-dia_nacimiento = '02'
-mes_nacimiento = '03'
+nombre = input ('Escriba tu Nombre: ')
+apellido1 = input ('Escriba tu Primer apellido: ')
+apellido2 = input ('Escriba tu segundo apellido: ')
+año_nacimiento = input('Escriba su año de nacimiento: ')
+dia_nacimiento = input('Escriba su día de nacimiento: ')
+mes_nacimiento =input ('Escriba su mes de nacimiento: ')
 
-print ('Cual es tu nombre?',nombre)
+print (nombre)
 
-print('Cual es tu primer apellido?',apellido1)
+print(apellido1)
 
-print ('Cual es tu segundo apellido?',apellido2)
+print (apellido2)
 
-print('En que año naciste?',año_nacimiento)
+print(año_nacimiento)
 
-print ('En que mes y día naciste?',mes_nacimiento,dia_nacimiento)
+print (mes_nacimiento,dia_nacimiento)
 
 nombre_Mayuscula = nombre.upper()+' '+apellido1.upper()+' '+apellido2.upper()
 nombre_minuscula = nombre.lower()+' '+apellido1.lower()+' '+apellido2.lower()
@@ -26,6 +26,7 @@ print ('Este es tu nombe completo en minuscula',nombre_minuscula)
 
 Fecha_Nacimiento =  dia_nacimiento+'-'+mes_nacimiento+'-'+año_nacimiento
 print('Esta es tu fecha de nacimiento', Fecha_Nacimiento)
+
 fecha_nacimiento = datetime.datetime.strptime(Fecha_Nacimiento, '%d-%m-%Y')
 edad = datetime.datetime.now().year - fecha_nacimiento.year
 
@@ -40,6 +41,9 @@ def contar_vocales(nombre_completo):
 
 cantidad = contar_vocales(nombre_completo)
 print('Tu nombre completo tiene',contar_vocales(nombre_completo),'vocales')
+
+
+
 
 def contar_letras(nombre_completo):
 	contador = 0
@@ -59,3 +63,9 @@ def mean(dataset):
     return sum(dataset) / len(dataset)
 
 print('El promedio de mi edad actual y mi edad en 20 años es',mean(media))
+
+print ('¿Tu edad es un carácter de tipo número?' ) 
+print(type(año_nacimiento))
+
+print ('¿Tu nombre completo es un carácter de tipo alfanumérico?' )
+print(type(nombre_completo))
